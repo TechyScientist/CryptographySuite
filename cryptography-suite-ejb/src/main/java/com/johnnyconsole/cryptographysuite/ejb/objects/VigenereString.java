bfda677c2de9871e02cbe3b23df0b7484fbf3a7d
Johnny Console <johnny.console@hotmail.com>
2024-10-08 15:36:27 -0400

    Add alphabet data to VigenereString

--- cryptography-suite-ejb/src/main/java/com/johnnyconsole/cryptographysuite/ejb/objects/VigenereString.java
+++ cryptography-suite-ejb/src/main/java/com/johnnyconsole/cryptographysuite/ejb/objects/VigenereString.java
@@ -4,11 +4,16 @@ import java.io.Serializable;
 
 public class VigenereString implements Serializable {
 
-    public String method, string;
+    public String method, string, alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public VigenereString(String method, String string) {
         this.method = method;
         this.string = string;
+    }
+
+    public VigenereString(String alphabet, String method, String string) {
+        this(method, string);
+        this.alphabet = alphabet;
     }
 
 }
