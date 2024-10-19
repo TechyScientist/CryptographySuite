9c0a39d7ccfc55ad640dc3f28b31523637b1a028
Johnny Console <johnny.console@hotmail.com>
2024-10-19 17:28:34 -0400

    Finish the Ohaver Implementation

---
+++ cryptography-suite-web/src/main/java/com/johnnyconsole/cryptographysuite/web/servlet/encoding/OhaverServlet.java
@@ -0,0 +1,41 @@
+package com.johnnyconsole.cryptographysuite.web.servlet.encoding;
+
+import com.johnnyconsole.cryptographysuite.ejb.interfaces.encoding.OhaverStatelessLocal;
+import com.johnnyconsole.cryptographysuite.ejb.objects.MorseString;
+
+import javax.ejb.EJB;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/OhaverServlet")
+public class OhaverServlet extends HttpServlet {
+
+    @EJB
+    private OhaverStatelessLocal stateless;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if(request.getParameter("ohaver-submit") != null) {
+            String message = request.getParameter("message").toUpperCase().replace(" ", "_"),
+                    encodeDecode = request.getParameter("encode-decode");
+
+            if(encodeDecode.equals("encode")) {
+                message = stateless.encode(message);
+            }
+            else {
+                message = stateless.decode(message);
+            }
+
+            HttpSession session = request.getSession();
+            session.setAttribute("ohaver", new MorseString(encodeDecode, message));
+            response.sendRedirect("ohaver.jsp");
+        }
+        else {
+            response.sendRedirect("index.jsp");
+        }
+    }
+}
