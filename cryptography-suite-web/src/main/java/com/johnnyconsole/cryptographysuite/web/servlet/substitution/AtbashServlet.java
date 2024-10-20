3c131c1ca6f6a8bb5a75c22fdd23c5556f71308d
Johnny Console <johnny.console@hotmail.com>
2024-10-20 10:02:49 -0400

    Create Atbash Servlet

---
+++ cryptography-suite-web/src/main/java/com/johnnyconsole/cryptographysuite/web/servlet/substitution/AtbashServlet.java
@@ -0,0 +1,36 @@
+package com.johnnyconsole.cryptographysuite.web.servlet.substitution;
+
+import com.johnnyconsole.cryptographysuite.ejb.interfaces.substitution.AtbashStatelessLocal;
+import com.johnnyconsole.cryptographysuite.ejb.objects.MorseString;
+
+import javax.ejb.EJB;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/AtbashServlet")
+public class AtbashServlet extends HttpServlet {
+
+    @EJB
+    private AtbashStatelessLocal stateless;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if(request.getParameter("atbash-submit") != null) {
+            String message = request.getParameter("message").toUpperCase(),
+                    encodeDecode = request.getParameter("encode-decode");
+
+            message = stateless.encodeDecode(message);
+
+            HttpSession session = request.getSession();
+            session.setAttribute("atbash", new MorseString(encodeDecode, message));
+            response.sendRedirect("atbash.jsp");
+        }
+        else {
+            response.sendRedirect("index.jsp");
+        }
+    }
+}
