b729877fbabacd802f49d56cb25213d4bb9585c8
Johnny Console <johnny.console@hotmail.com>
2025-04-02 11:52:08 -0400

    Modify EncodedString object to hold encryption/decryption keys

--- cryptography-suite-ejb/src/main/java/com/johnnyconsole/cryptographysuite/ejb/objects/EncodedString.java
+++ cryptography-suite-ejb/src/main/java/com/johnnyconsole/cryptographysuite/ejb/objects/EncodedString.java
@@ -4,11 +4,16 @@ import java.io.Serializable;
 
 public class EncodedString implements Serializable {
 
-    public String method, string;
+    public String method, string, key;
 
     public EncodedString(String method, String string) {
         this.method = method;
         this.string = string;
+    }
+
+    public EncodedString(String method, String string, String key) {
+        this(method, string);
+        this.key = key;
     }
 
 }
