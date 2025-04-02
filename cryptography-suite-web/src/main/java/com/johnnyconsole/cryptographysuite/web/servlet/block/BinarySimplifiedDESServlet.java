920d70cfb768d1d52d4739e36b74307d718cd64f
Johnny Console <johnny.console@hotmail.com>
2025-04-02 11:53:11 -0400

    Create servlet to handle encryption and decryption of binary string messages with SDES

---
+++ cryptography-suite-web/src/main/java/com/johnnyconsole/cryptographysuite/web/servlet/block/BinarySimplifiedDESServlet.java
@@ -0,0 +1,55 @@
+package com.johnnyconsole.cryptographysuite.web.servlet.block;
+
+import com.johnnyconsole.cryptographysuite.ejb.interfaces.block.SimplifiedDESStatelessLocal;
+import com.johnnyconsole.cryptographysuite.ejb.objects.EncodedString;
+
+import javax.ejb.EJB;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/BinarySDESServlet")
+public class BinarySimplifiedDESServlet extends HttpServlet {
+
+    @EJB
+    private SimplifiedDESStatelessLocal stateless;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if(request.getParameter("binary-sdes-submit") != null) {
+            String message = request.getParameter("message"),
+                    key = request.getParameter("key"),
+                    encodeDecode = request.getParameter("encode-decode"),
+                    result = "";
+
+            if (key.length() != 10) {
+                response.sendRedirect("binary-sdes.jsp?error=key&key=" + key);
+            } else {
+
+                while (message.length() % 8 != 0) {
+                    message += '\0';
+                }
+
+                if (encodeDecode.equals("encipher")) {
+                    for(int i = 0; i < message.length(); i += 8) {
+                        result += stateless.encrypt(message.substring(i, i + 8), key);
+                    }
+                }
+                else {
+                    for (int i = 0; i < message.length(); i += 8) {
+                        result += stateless.decrypt(message.substring(i, i + 8), key);
+                    }
+                }
+                HttpSession session = request.getSession();
+                session.setAttribute("sdes", new EncodedString(encodeDecode, result, key));
+                response.sendRedirect("binary-sdes.jsp");
+            }
+        }
+        else {
+            response.sendRedirect("index.jsp");
+        }
+    }
+}
