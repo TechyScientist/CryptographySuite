7099c69ee5490228efc1e90b67ec7d7e66524bce
Johnny Console <johnny.console@hotmail.com>
2025-04-20 18:28:06 -0400

    Add support for CBC encryption/decryption

--- cryptography-suite-web/src/main/java/com/johnnyconsole/cryptographysuite/web/servlet/block/BinarySimplifiedDESServlet.java
+++ cryptography-suite-web/src/main/java/com/johnnyconsole/cryptographysuite/web/servlet/block/BinarySimplifiedDESServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet("/BinarySDESServlet")
 public class BinarySimplifiedDESServlet extends HttpServlet {
@@ -17,12 +18,17 @@ public class BinarySimplifiedDESServlet extends HttpServlet {
     @EJB
     private SimplifiedDESStatelessLocal stateless;
 
+    private ArrayList<String> plaintext = new ArrayList<>(),
+                        ciphertext = new ArrayList<>();
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         if(request.getParameter("binary-sdes-submit") != null) {
             String message = request.getParameter("message"),
                     key = request.getParameter("key"),
                     encodeDecode = request.getParameter("encode-decode"),
+                    mode=request.getParameter("mode"),
+                    iv = "",
                     result = "";
 
             if (key.length() != 10) {
@@ -33,18 +39,33 @@ public class BinarySimplifiedDESServlet extends HttpServlet {
                     message += '\0';
                 }
 
-                if (encodeDecode.equals("encipher")) {
-                    for(int i = 0; i < message.length(); i += 8) {
-                        result += stateless.ecb_encrypt(message.substring(i, i + 8), key);
+                if(mode.equals("ECB")) {
+                    if (encodeDecode.equals("encipher")) {
+                        for (int i = 0; i < message.length(); i += 8) {
+                            result += stateless.ecb_encrypt(message.substring(i, i + 8), key);
+                        }
+                    } else {
+                        for (int i = 0; i < message.length(); i += 8) {
+                            result += stateless.ecb_decrypt(message.substring(i, i + 8), key);
+                        }
                     }
-                }
-                else {
-                    for (int i = 0; i < message.length(); i += 8) {
-                        result += stateless.ecb_decrypt(message.substring(i, i + 8), key);
+                } else {
+                    iv = request.getParameter("iv");
+
+                    if (encodeDecode.equals("encipher")) {
+                        for (int i = 0, block = 0; i < message.length(); i += 8, block++) {
+                            ciphertext.add(stateless.cbc_encrypt(message.substring(i, i + 8), i == 0 ? iv : ciphertext.get(block - 1), key));
+                            result += ciphertext.get(block);
+                        }
+                    } else {
+                        for (int i = 0, block = 0; i < message.length(); i += 8, block++) {
+                            ciphertext.add(message.substring(i, i + 8));
+                            result += stateless.cbc_decrypt(message.substring(i, i + 8), i == 0 ? iv : ciphertext.get(block - 1), key);
+                        }
                     }
                 }
                 HttpSession session = request.getSession();
-                session.setAttribute("sdes", new EncodedString(encodeDecode, result, key));
+                session.setAttribute("sdes", new EncodedString(encodeDecode, result, key, mode, iv));
                 response.sendRedirect("binary-sdes.jsp");
             }
         }
