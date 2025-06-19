0eb38bdea9d2ae4522b7a5229c078aa9d4c2f904
Johnny Console <johnny.console@hotmail.com>
2025-06-19 10:55:38 -0400

    Add bit length options to NumberSequence object spec

--- cryptography-suite-ejb/src/main/java/com/johnnyconsole/cryptographysuite/ejb/objects/NumberSequence.java
+++ cryptography-suite-ejb/src/main/java/com/johnnyconsole/cryptographysuite/ejb/objects/NumberSequence.java
@@ -3,10 +3,12 @@ package com.johnnyconsole.cryptographysuite.ejb.objects;
 public class NumberSequence {
 
     public String base;
+    public int bitLength;
     public int[] sequence;
 
-    public NumberSequence(String base, int[] sequence) {
+    public NumberSequence(String base, int bitLength, int[] sequence) {
         this.base = base;
+        this.bitLength = bitLength;
         this.sequence = sequence;
     }
 
