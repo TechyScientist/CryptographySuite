4354147106f7d705860bba45cbbc08f006885292
Johnny Console <johnny.console@hotmail.com>
2025-06-19 12:38:06 -0400

    Replace unnecessary mod method

--- cryptography-suite-ejb/src/main/java/com/johnnyconsole/cryptographysuite/ejb/impl/prng/BlumBlumShubStateles.java
+++ cryptography-suite-ejb/src/main/java/com/johnnyconsole/cryptographysuite/ejb/impl/prng/BlumBlumShubStateles.java
@@ -14,12 +14,12 @@ public class BlumBlumShubStateles implements BlumBlumShubStatelessLocal {
 
         int n = p * q;
         String[] seq = new String[length];
-        seed = mod((seed * seed), n);
+        seed = Math.floorMod((seed * seed), n);
         for(int i = 0; i < length; i++) {
             StringBuilder bin = new StringBuilder();
             for(int bit = 0; bit < bitLength; bit++) {
-                seed = mod((seed * seed), n);
-                bin.append(mod((seed * seed), 2));
+                seed = Math.floorMod((seed * seed), n);
+                bin.append(Math.floorMod((seed * seed), 2));
             }
             seq[i] = bin.toString();
         }
@@ -31,12 +31,12 @@ public class BlumBlumShubStateles implements BlumBlumShubStatelessLocal {
         if(!check(bitLength, p, q, seed, length)) return null;
         int n = p * q;
         int[] seq = new int[length];
-        seed = mod((seed * seed), n);
+        seed = Math.floorMod((seed * seed), n);
         for(int i = 0; i < length; i++) {
             StringBuilder bin = new StringBuilder();
             for(int bit = 0; bit < bitLength; bit++) {
-                seed = mod((seed * seed), n);
-                bin.append(mod((seed * seed), 2));
+                seed = Math.floorMod((seed * seed), n);
+                bin.append(Math.floorMod((seed * seed), 2));
             }
             seq[i] = Integer.parseInt(bin.toString(), 2);
         }
@@ -61,12 +61,12 @@ public class BlumBlumShubStateles implements BlumBlumShubStatelessLocal {
             error = "q = " + q + " is not prime";
             return false;
         }
-        if(mod(p, 4) != 3) {
-            error = "p = " + p + " is not 3 mod 4: is " + mod(p, 4) + " mod 4";
+        if(Math.floorMod(p, 4) != 3) {
+            error = "p = " + p + " is not 3 mod 4: is " + Math.floorMod(p, 4) + " mod 4";
             return false;
         }
-        if(mod(q, 4) != 3) {
-            error = "q = " + q + " is not 3 mod 4:  is " + mod(q, 4) + " mod 4";
+        if(Math.floorMod(q, 4) != 3) {
+            error = "q = " + q + " is not 3 mod 4:  is " + Math.floorMod(q, 4) + " mod 4";
             return false;
         }
         long n = (long) p * q;
@@ -85,22 +85,16 @@ public class BlumBlumShubStateles implements BlumBlumShubStatelessLocal {
         return true;
     }
 
-    private long mod(long a, long b) {
-        if(a < 0) return mod(a + b, b);
-        if(a < b) return a;
-        return mod(a - b, b);
-    }
-
     private long gcd(long a, long b) {
         for(long n = Math.min(a, b); n > 1; n--) {
-            if(mod(a, b) == 0 && mod(a, b) == 0) return n;
+            if(Math.floorMod(a, n) == 0 && Math.floorMod(b, n) == 0) return n;
         }
         return 1;
     }
 
     private boolean isPrime(long n) {
         for(int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) return false;
+            if (Math.floorMod(n, i) == 0) return false;
         }
         return true;
     }
