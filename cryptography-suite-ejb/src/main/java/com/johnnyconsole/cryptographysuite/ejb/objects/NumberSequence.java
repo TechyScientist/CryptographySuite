4c638724c8c2526bf42f56d595c903beaac58f03
Johnny Console <johnny.console@hotmail.com>
2025-06-19 10:59:27 -0400

    Create different constructor to handle binary sequences

--- cryptography-suite-ejb/src/main/java/com/johnnyconsole/cryptographysuite/ejb/objects/NumberSequence.java
+++ cryptography-suite-ejb/src/main/java/com/johnnyconsole/cryptographysuite/ejb/objects/NumberSequence.java
@@ -3,13 +3,26 @@ package com.johnnyconsole.cryptographysuite.ejb.objects;
 public class NumberSequence {
 
     public String base;
-    public int bitLength;
-    public int[] sequence;
+    public int p, q, seed, bitLength;
+    public int[] intSequence;
+    public String[] strSequence;
 
-    public NumberSequence(String base, int bitLength, int[] sequence) {
-        this.base = base;
+    public NumberSequence(int p, int q, int seed, int bitLength, int[] sequence) {
+        this.base = "Decimal";
+        this.p = p;
+        this.q = q;
+        this.seed = seed;
         this.bitLength = bitLength;
-        this.sequence = sequence;
+        this.intSequence = sequence;
+    }
+
+    public NumberSequence(int p, int q, int seed, int bitLength, String[] sequence) {
+        this.base = "Binary";
+        this.p = p;
+        this.q = q;
+        this.seed = seed;
+        this.bitLength = bitLength;
+        this.strSequence = sequence;
     }
 
 }
