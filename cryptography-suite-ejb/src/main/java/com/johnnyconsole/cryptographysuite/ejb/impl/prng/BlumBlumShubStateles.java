ffa87883fdaa4d901a76c316eca314a69832c06e
Johnny Console <johnny.console@hotmail.com>
2025-06-19 11:26:52 -0400

    Expose the error method of BlumBlumShubStateles

--- cryptography-suite-ejb/src/main/java/com/johnnyconsole/cryptographysuite/ejb/impl/prng/BlumBlumShubStateles.java
+++ cryptography-suite-ejb/src/main/java/com/johnnyconsole/cryptographysuite/ejb/impl/prng/BlumBlumShubStateles.java
@@ -43,6 +43,7 @@ public class BlumBlumShubStateles implements BlumBlumShubStatelessLocal {
         return seq;
     }
 
+    @Override
     public String error() {
         return error;
     }
--- cryptography-suite-ejb/src/main/java/com/johnnyconsole/cryptographysuite/ejb/interfaces/prng/BlumBlumShubStatelessLocal.java
+++ cryptography-suite-ejb/src/main/java/com/johnnyconsole/cryptographysuite/ejb/interfaces/prng/BlumBlumShubStatelessLocal.java
@@ -7,5 +7,6 @@ public interface BlumBlumShubStatelessLocal {
 
     String[] generateBinarySequence(int bitLength, int p, int q, long seed, int length);
     int[] generateDecimalSequence(int bitLength, int p, int q, long seed, int length);
+    String error();
 
 }
