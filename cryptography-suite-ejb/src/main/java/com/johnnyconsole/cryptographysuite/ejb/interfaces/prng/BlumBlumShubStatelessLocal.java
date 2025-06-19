4fa3258f0164022e9744c90c9282f446e952f5ee
Johnny Console <johnny.console@hotmail.com>
2025-06-19 10:56:07 -0400

    Create bean interface for Local Stateless BlumBlumShub Bean

---
+++ cryptography-suite-ejb/src/main/java/com/johnnyconsole/cryptographysuite/ejb/interfaces/prng/BlumBlumShubStatelessLocal.java
@@ -0,0 +1,11 @@
+package com.johnnyconsole.cryptographysuite.ejb.interfaces.prng;
+
+import javax.ejb.Local;
+
+@Local
+public interface BlumBlumShubStatelessLocal {
+
+    String[] generateBinarySequence(int bitLength, int p, int q, long seed, int length);
+    int[] generateDecimalSequence(int bitLength, int p, int q, long seed, int length);
+
+}
